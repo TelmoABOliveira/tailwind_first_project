c42db645f78157808d2be3f437ffba2bb9ac1816
TelmoABOliveira <telmo_oliveira21@hotmail.com>
2025-01-22 12:35:06 +0000

    Basic structure

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import SpecialOffer from "./SpecialOffer";
+import SuperQuality from "./SuperQuality";
+import PopularProduct from "./PopularProduct";
+import CustomerReviews from "./CustomerReviews";
+
+export {
+    Hero,
+    Footer,
+    Services,
+    Subscribe,
+    SpecialOffer,
+    SuperQuality,
+    PopularProduct,
+    CustomerReviews
+}
